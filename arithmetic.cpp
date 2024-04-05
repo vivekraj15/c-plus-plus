7be89dbf2977dc226f1a514372c53b0e319eb420
vivekraj15 <166148015+vivekraj15@users.noreply.github.com>
2024-04-05 20:51:37 +0530

    Add files via upload

---
+++ arithmetic.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b, add, sub, mul, div, mod;
+    cout<<"Enter the first number";
+    cin>>a;
+    cout<<"Enter the second number";
+    cin>>b;
+    add=a+b;
+    sub=a-b;
+    mul=a*b;
+    div=a/b;
+    mod=a%b;
+     cout<<add<<endl;
+     cout<<sub<<endl;
+     cout<<mul<<endl;
+     cout<<div<<endl;
+     cout<<mod<<endl;
+     return 0;
+}
